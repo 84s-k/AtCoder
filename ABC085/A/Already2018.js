80dbee3fdfa04eafbfd63550561b4a8a8ff5a51c
hi-ra-ya-ma <hirayama.no.uni@gmail.com>
2020-06-10 07:56:23 +0900

    [update]ABC085 A
    入力をreplaceで変換して出力するのが良さそう。

--- ABC085/A/Already2018.js
+++ ABC085/A/Already2018.js
@@ -1,3 +1,2 @@
-const input = require("fs").readFileSync("/dev/stdin", "utf8").split("/");
-input[0] = "2018";
-console.log(input[0] + "/" + input[1] + "/" + input[2]);
+const input = require("fs").readFileSync("/dev/stdin", "utf8").split(/\n/);
+console.log(input[0].replace("2017", "2018"));
