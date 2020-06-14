6d99ef1076fdf8d0947416aee99c64b4e4b7c989
hi-ra-ya-ma <hirayama.no.uni@gmail.com>
2020-06-15 06:44:51 +0900

    [update]ABC087 A コードを簡潔に
    残金の取得は余りを取ってあげればOK

--- ABC087/A/BuyingSweets.ts
+++ ABC087/A/BuyingSweets.ts
@@ -2,9 +2,5 @@ import * as fs from "fs";
 const input = (fs.readFileSync("/dev/stdin", "utf8") as string)
   .split("\n")
   .map(Number);
-let remainingMoney: number = input[0];
-remainingMoney -= input[1];
-const donutsCount: number = Math.floor(remainingMoney / input[2]);
-remainingMoney -= input[2] * donutsCount;
-
+const remainingMoney: number = (input[0] - input[1]) % input[2];
 console.log(remainingMoney);
